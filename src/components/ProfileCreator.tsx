544e2a70e97646533a7c4091e703da851a6aedb2
Tyson <tywalk10@gmail.com>
2021-12-03 13:27:23 -0500

    profile creator

--- src/components/ProfileCreator.tsx
+++ src/components/ProfileCreator.tsx
@@ -1,19 +1,36 @@
-import { FormEvent } from "react";
+import { FormEvent, useContext, useRef, useState } from "react";
+import AuthContext from "../context/AuthContext";
 import Profile from "../models/Profile";
 import { addProfile } from "../services/ProfileService";
 import "./ProfileCreator.css";
+import { storage } from "../firebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const ProfileCreator = () => {
   // const [username, setUsername]
+  const { user } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (e: FormEvent) => {
     e.preventDefault();
     const profile: Profile = {
-      uid: "blah",
-      avatar: "lkasjdfois",
-      username: "asidjdf",
+      uid: user!.uid,
+      avatar: fileInputRef,
+      username,
     };
-    addProfile;
+    const files = fileInputRef.current?.files;
+    if (files && files[0]) {
+      const file = files[0]; // Here is the file we need
+      const storageRef = ref(storage, file.name);
+      uploadBytes(storageRef, file).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((response) => {
+          console.log(response);
+          profile.avatar = response;
+          addProfile(profile);
+        });
+      });
+    }
   };
 
   return (
@@ -23,8 +40,20 @@ const ProfileCreator = () => {
         <input type="text" name="username" id="username" />
       </label>
       <label htmlFor=""></label>
+      <input ref={fileInputRef} type="file" />
     </form>
   );
 };
 
 export default ProfileCreator;
+function ref(storage: any, name: string) {
+  throw new Error("Function not implemented.");
+}
+
+function uploadBytes(storageRef: void, file: File) {
+  throw new Error("Function not implemented.");
+}
+
+function getDownloadURL(ref: any) {
+  throw new Error("Function not implemented.");
+}
