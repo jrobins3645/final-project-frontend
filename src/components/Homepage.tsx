cd1ac281ce88b7afebeed764f1683e1a69a3c049
Tyson <tywalk10@gmail.com>
2021-12-14 09:04:40 -0500

    css

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import Popup from "./Popup";
 import { Link } from "react-router-dom";
 import TriviaContext from "../context/TriviaContext";
+import cardImage from "../images/cardImage.png";
 
 const Homepage = () => {
   const {
@@ -23,13 +24,20 @@ const Homepage = () => {
 
   return (
     <div className="Homepage">
-      <div className="quiz-container">
-        <h3>Original 151 Pokemon Quiz</h3>
-        <p>Type the name of the pokemon that is shown.</p>
-        <p>You will have x time to answer as many questions as you can.</p>
-        <Link to="/quiz">
-          <button onClick={() => newQuiz()}>Start Quiz!</button>
-        </Link>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img className="card" src={cardImage} alt="card-pic" />
+          </div>
+          <div className="flip-card-back">
+            <h3>Original 151 Pokemon Quiz</h3>
+            <p>Type the name of the pokemon that is shown.</p>
+            <p>You will have x time to answer as many questions as you can.</p>
+            <Link to="/quiz">
+              <button onClick={() => newQuiz()}>Start Quiz!</button>
+            </Link>
+          </div>
+        </div>
       </div>
       {user && !profile ? <Popup /> : null}
     </div>
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -119,7 +119,7 @@ const Quiz = () => {
                     autoFocus
                   />
                   <button
-                    id="I-dont-know-button"
+                    className="I-dont-know-button"
                     onClick={() => setCounter((prev) => prev + 1)}
                   >
                     I don't know
@@ -128,7 +128,7 @@ const Quiz = () => {
               </div>
             </form>
             <Link to="/">
-              <button>Quit Quiz</button>
+              <button className="quit-button">Quit Quiz</button>
             </Link>
           </div>
         </>
